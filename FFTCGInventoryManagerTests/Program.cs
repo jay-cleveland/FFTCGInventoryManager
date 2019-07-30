ae0e970e25cc480e798e4084450339a98856da6d
jay-cleveland <jaycee.cleveland@outlook.com>
2019-07-30 13:46:37 -0500

    Created new test project and deleted default ValuesController

---
+++ FFTCGInventoryManagerTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FFTCGInventoryManagerTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
