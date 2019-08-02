f782eee4b5dd37f0c5f63800fb238174aa939b37
jay-cleveland <jaycee.cleveland@outlook.com>
2019-08-02 17:50:38 -0500

    Finished writing tests for InventoryService.cs

--- FFTCGInventoryManagerTest/InventoryServiceTests.cs
+++ FFTCGInventoryManagerTest/InventoryServiceTests.cs
@@ -77,5 +77,52 @@ namespace FFTCGInventoryManagerTest
             Assert.Throws<Exception>(() => inventoryService.RemoveCard(inventoryId, CardId));
             repository.Verify(r => r.RemoveCard(inventoryId, CardId), Times.Never);
         }
+
+        [Fact]
+        public void CreateNewInventory_Successful()
+        {
+            //arrange
+            var repository = new Mock<IInventoryRepository>();
+            repository.Setup(r => r.CreateNewInventory());
+
+            //act
+            var inventoryService = new InventoryService(repository.Object);
+            inventoryService.CreateNewInventory();
+
+            //assert
+            repository.Verify(r => r.CreateNewInventory(), Times.Once);
+        }
+
+        [Fact]
+        public void DeleteInventory_InvalidInputs_Throws()
+        {
+            //arrange
+            var inventoryId = Guid.NewGuid().ToString();
+            var repository = new Mock<IInventoryRepository>();
+            repository.Setup(r => r.GetInventory(inventoryId)).Returns((Inventory)null);
+
+            //act
+            var inventoryService = new InventoryService(repository.Object);
+
+            //assert
+            Assert.Throws<Exception>(() => inventoryService.DeleteInventory(inventoryId));
+            repository.Verify(r => r.DeleteInventory(inventoryId), Times.Never);
+        }
+
+        [Fact]
+        public void DeleteInventory_ValidInputs_Successful()
+        {
+            //arrange
+            var inventoryId = Guid.NewGuid().ToString();
+            var repository = new Mock<IInventoryRepository>();
+            repository.Setup(r => r.GetInventory(inventoryId)).Returns(new Inventory(inventoryId, new List<Card>()));
+
+            //act
+            var inventoryService = new InventoryService(repository.Object);
+            inventoryService.DeleteInventory(inventoryId);
+
+            //assert
+            repository.Verify(r => r.DeleteInventory(inventoryId), Times.Once);
+        }
     }
 }
